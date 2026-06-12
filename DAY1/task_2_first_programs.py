3a5a2203c1c30cae528a6e0e7dada60db7de4094
ruhinvahora68-lang <ruhinvahora68@gmail.com>
2026-06-12 03:59:06 -0700

    DAY8 is create

---
+++ DAY1/task_2_first_programs.py
@@ -0,0 +1,8 @@
+# Printing a simple message (using double quotes)
+print("Hello World")
+
+# Printing another message
+print("Welcome to Python")
+
+# Printing mathematical addition (Notice we do not use quotes for numbers!)
+print(10 + 20)
